72aea66a70e00273b8950735bcc5aa7825c7b86f
Daniel <Daniel@Daniel.home>
2016-04-20 11:43:04 +0200

    account .

--- eshop1/src/valueobjects/Account.java
+++ eshop1/src/valueobjects/Account.java
@@ -7,7 +7,6 @@ package valueobjects;
 public class Account {
 
 	// Attribute zur Beschreibung des Accounts
-	//??? ???
 
 	protected int accountNr;
 	protected String accountname;
@@ -19,8 +18,8 @@ public class Account {
 		this.accountNr = accnummer;
 	}
 
-	//?Ã¼berprÃ¼ft ob der Account schon vorhanden ist
-	@Override
+	//Überprüfe ob der Account schon vorhanden ist
+	
 	public boolean equals(Object obj) {
 		if (obj instanceof Account) {
 			Account anderesAccount = (Account) obj;
@@ -31,65 +30,30 @@ public class Account {
 	}
 
 	// Getter und Setter
-
-	/**
-	 * Methode zum Abrufen der Accountnummer
-	 * 
-	 * @return accountNr Accountnummer
-	 */
-
 	public int getAccountNr() {
 		return accountNr;
 	}
 
-	/**
-	 * Methode zum Setzen der Accountnummer
-	 * 
-	 * @param accountNr
-	 *            Accountnummer
-	 */
 
 	public void setAccountNr(int accountNr) {
 		this.accountNr = accountNr;
 	}
 
-	/**
-	 * Methode zum Abrufen des Accountnamen
-	 * 
-	 * @return accountname Accoutname
-	 */
-
+	
 	public String getName() {
 		return accountname;
 	}
 
-	/**
-	 * Methode zum Setzer des Accountnamen
-	 * 
-	 * @param name
-	 *            Accoutnname
-	 */
 
 	public void setName(String name) {
 		this.accountname = name;
 	}
 
-	/**
-	 * Methode zum Abrufen des Passworts
-	 * 
-	 * @return accountpasswort
-	 */
 
 	public String getPasswort() {
 		return accountpasswort;
 	}
-
-	/**
-	 * Methode zum Setzen des Passworts
-	 * 
-	 * @param passwort
-	 *            Accountpasswort
-	 */
+	
 
 	public void setPasswort(String passwort) {
 		this.accountpasswort = passwort;
