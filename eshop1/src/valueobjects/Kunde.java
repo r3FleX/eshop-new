d60ec70fd96ddff462c59430c8e78ad06fa01c23
Daniel <Daniel@Daniel.home>
2016-04-20 12:27:16 +0200

    Kunde

--- eshop1/src/valueobjects/Kunde.java
+++ eshop1/src/valueobjects/Kunde.java
@@ -10,10 +10,10 @@ public class Kunde extends Account {
 	private String wohnort;
 	private Warenkorb warenkorb = new Warenkorb();
 
-	public Kunde(String name, String passwort, int accnummer, String strasse1, int plz1, String ort) {
+	public Kunde(String name, String passwort, int accnummer, String strasse, int plz, String ort) {
 		super(name, passwort, accnummer);
-		this.strasse = strasse1;
-		this.plz = plz1;
+		this.strasse = strasse;
+		this.plz = plz;
 		this.wohnort = ort;
 	}
 
