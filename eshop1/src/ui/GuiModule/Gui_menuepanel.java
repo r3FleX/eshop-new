ebfe71dda2cc45e4a138640263aa0097e31343ec
Daniel <Daniel@Daniel.home>
2016-05-23 01:59:38 +0200

    menuepanel test

---
+++ eshop1/src/ui/GuiModule/Gui_menuepanel.java
@@ -0,0 +1,15 @@
+package ui.GuiModule;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class Gui_menuepanel implements ActionListener{
+
+	private JPanel menuepanel;
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
--- eshop1/src/ui/GuiModule/Gui_menupanel.java
+++
@@ -1,5 +0,0 @@
-package ui.GuiModule;
-
-public class Gui_menupanel {
- //commit and cry
-}
